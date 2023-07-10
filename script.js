e8b67a3fd195febf4a2b2dc15a1316c78f11b84f
Hüseyin Türköz <59177691+hsyntrkz87@users.noreply.github.com>
2023-07-10 15:48:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const url = document.querySelector("input");
+const downloadBtn = document.querySelector("button");
+
+
+downloadBtn.addEventListener("click", async() => {
+
+try {
+    
+    const response = await fetch(url.value);
+    const file = await response.blob();
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(file);
+    link.download = new Date().getTime();
+    link.click();
+
+} catch (error) {
+    alert("Failed");
+}
+
+
+});
